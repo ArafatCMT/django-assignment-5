890657b6d346c34e79e0f662835428b613cb11e6
ArafatCMT <arafatemon6969@gmail.com>
2024-06-26 09:28:46 +0600

    changes done

--- library_management_system/settings.py
+++ library_management_system/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['library-system-qyfa.onrender.com',]
+ALLOWED_HOSTS = ["*"]
+CSRF_TRUSTED_ORIGINS = ['https://library-system-qyfa.onrender.com']
 
 
 # Application definition
