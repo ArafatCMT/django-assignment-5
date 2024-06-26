a11a750419792a7903db78eb466501a7bd74967c
ArafatCMT <arafatemon6969@gmail.com>
2024-06-26 08:48:33 +0600

    Allowed hosts added

--- library_management_system/settings.py
+++ library_management_system/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['198.211.99.20',]
 
 
 # Application definition
